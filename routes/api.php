857bd4550b1657d6e58b5c49d0deff871698d72d
WebGCDev <gaetano.chi@gmail.com>
2024-02-15 19:41:45 +0100

    Add: Milestone 1 Completed

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Project;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Esempio lezione
+// Route::get('project',function(){
+//     $project = Project::all();
+
+//         return response()->json($project);
+// });
+
+Route::get('project',[ProjectController::class, 'index']);
